b25e4ac69a0b254642525f511dae7d4a7cb8c9d3
CorruptedData404 <corrupteddata404@hotmail.com>
2025-05-04 21:32:21 -0700

    Begun implementing ice charges

---
+++ src/main/java/ca/corruptdata/moodyghasts/item/ModItems.java
@@ -0,0 +1,19 @@
+package ca.corruptdata.moodyghasts.item;
+
+import ca.corruptdata.moodyghasts.MoodyGhasts;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MoodyGhasts.MOD_ID);
+    public static final DeferredItem<Item> ICE_CHARGE = ITEMS.registerSimpleItem("ice_charge", new Item.Properties());
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
